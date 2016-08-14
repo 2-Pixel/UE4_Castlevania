e8a0cbfa9040f66638f53d612516faf5c3820e4d
Jon Shamber <shamthemaster@gmail.com>
2016-08-13 19:49:12 -0700

    Blueprint Rollback
    Took out the C++ option

--- CastlevaniaRedux/Source/CastlevaniaRedux/CastlevaniaRedux.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#include "CastlevaniaRedux.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CastlevaniaRedux, "CastlevaniaRedux" );
- 
